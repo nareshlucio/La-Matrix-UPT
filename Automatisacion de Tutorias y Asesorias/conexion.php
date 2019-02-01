4c3ce4909e627d0d4998e9ae5dc9a54d3c1f0803
David Naresh <40779474+Poti7652@users.noreply.github.com>
2019-01-31 20:40:15 -0600

    Delete conexion.php

--- Automatisacion de Tutorias y Asesorias/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$server="localhost";
-$user="root";
-$db="automatizacion";
-$con= mysqli_connect($server,$user,"",$db) or die ("No se ha podido conectar con el servidor de Base de datos");
- ?>
